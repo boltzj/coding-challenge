7098e52ebdba3ec5fc8e064e9f72023d50ab5a96
Jérôme BOLTZ <boltz.jerome@gmail.com>
2014-08-02 20:30:07 -0700

    Question3

--- question3/q3.py
+++ question3/q3.py
@@ -1,1 +1,21 @@
 __author__ = 'boltz_j'
+
+import re
+
+data = dict()
+
+for line in open('test.log'):
+    buffer = line.split()
+    # service = buffer[4].split('[')[0]
+    service = re.search("(.*?)\[(.*?)\]", buffer[4]).group(1)
+    user = re.search("\((.*?)\)", buffer[5]).group(1)
+    command = buffer[6]
+
+    if (service, user, command) not in data:
+        data[(service, user, command)] = 1
+    else:
+        data[(service, user, command)] += 1
+
+
+for elt in data:
+    print(elt[0], elt[1], elt[2], data[elt])
